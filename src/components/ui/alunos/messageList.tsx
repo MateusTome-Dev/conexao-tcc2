83b88769e6b078d21e3c9ce2fab5b0d9970ac93e
Mateus Tomé <154273310+MateusTome-Dev@users.noreply.github.com>
2025-05-08 14:05:57 -0300

    Revert "Merge branch 'master' into CorrectEditTeacher"
    This reverts commit ee56211e0c6be3aca91437ca69a838b3d16eae22, reversing
    changes made to 00a077b8fc5d2894f8adf049ab6726adf9f8f062.

--- src/components/ui/alunos/messageList.tsx
+++ src/components/ui/alunos/messageList.tsx
@@ -64,7 +64,7 @@ function MessageList({ className }: { className?: string }) {
     const fetchMessages = async () => {
       try {
         const reminderResponse = await fetch(
-          `https://backendona-amfeefbna8ebfmbj.eastus2-01.azurewebsites.net/api/reminder/${turma}`
+          `http://localhost:3000/api/reminder/${turma}`
         );
   
         if (!reminderResponse.ok)
