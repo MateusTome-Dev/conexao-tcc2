faad4b4cf421613a033dec69bfbc814eaa2af108
Rafael Augusto <148670173+RafaAugustoFS@users.noreply.github.com>
2025-05-22 14:37:31 -0300

    🌘
    Adicionando modo dark nos modais

--- src/components/modals/modalCreate.tsx
+++ src/components/modals/modalCreate.tsx
@@ -11,9 +11,9 @@ export default function ModalCreate({ isOpen, onClose, message }: ModalProps) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
-        <span className="animate-spin border-4 border-blue-500 border-t-transparent rounded-full w-10 h-10"></span>
-        <p className="mt-4">{message || "Carregando..."}</p>
+      <div className="bg-white dark:bg-[#141414] p-6 rounded-lg shadow-lg flex flex-col items-center">
+        <span className="animate-spin dark:text-blue-500 dark:t border-4 border-blue-500 border-t-transparent rounded-full w-10 h-10"></span>
+        <p className="mt-4 dark:text-white">{message || "Carregando..."}</p>
         <button
           onClick={onClose}
           className="mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
