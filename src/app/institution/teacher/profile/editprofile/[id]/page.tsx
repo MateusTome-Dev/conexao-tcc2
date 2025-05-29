32e09fad528bacb5d347d8efe332314c3f231f20
Gaby Brito <148670173+RafaAugustoFS@users.noreply.github.com>
2025-05-29 16:07:15 -0300

    🔨
    Corrigindo informações do teacher.

--- src/app/institution/teacher/profile/editprofile/[id]/page.tsx
+++ src/app/institution/teacher/profile/editprofile/[id]/page.tsx
@@ -156,7 +156,7 @@ export default function TeacherProfileEdit() {
       setLoading(true);
       try {
         const response = await fetch(
-          `https://backendona-amfeefbna8ebfmbj.eastus2-01.azurewebsites.net/api/teacher/${id}`
+          `https://onacademy-e2h7csembwhrf2bu.brazilsouth-01.azurewebsites.net/api/teacher/${id}`
         );
         if (!response.ok) throw new Error("Falha ao buscar dados do professor");
 
